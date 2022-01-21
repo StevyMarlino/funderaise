a283a3b30b788b3f306b10b6ad0c60ed5ba294a2
anyepeter <74084998+anyepeter@users.noreply.github.com>
2022-01-21 14:39:35 +0100

    rotating navbar

---
+++ view/html/script.js
@@ -0,0 +1,8 @@
+const open = document.getElementById('open')
+const close = document.getElementById('close')
+const container = document.querySelector('.container0')
+
+open.addEventListener('click', () => container.classList.add
+('show-nav'))
+close.addEventListener('click', () => container.classList.remove
+('show-nav'))
