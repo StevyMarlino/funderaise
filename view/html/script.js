573715dd5aaec8980bdf5b34688e77707e0886be
stevy Endaman <stevyabessolo@gmail.com>
2022-01-25 10:07:57 +0100

    delete

--- view/html/script.js
+++
@@ -1,8 +0,0 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container0 = document.querySelector('.container0')
-
-open.addEventListener('click', () => container0.classList.add
-('show-nav'))
-close.addEventListener('click', () => container0.classList.remove
-('show-nav'))  
